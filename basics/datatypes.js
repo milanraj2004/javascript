5adfd4cd51b00797ad2d6846f48355b1878c73bb
milanraj2004 <157353158+milanraj2004@users.noreply.github.com>
2025-04-18 06:56:23 +0000

    dataype

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS Code as newer Version
+
+//alert(3+9) // we are using Node js not browser
+console.log(3+3);
+console.log("Hitesh")
+let name ="milan"
+let age = 19
+let ageisValid =true
+let state;
+// null => standalone value
+//undefined => maine define nhi kiya or 
+//null kanka mtlb khali
+//symbol => unique
+
+//object 
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
