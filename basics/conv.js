66e8b4b4e1ee8dbdaed93983823b2dcf4f45d8b5
milanraj2004 <157353158+milanraj2004@users.noreply.github.com>
2025-04-18 07:33:49 +0000

    conversion of datatype

---
+++ basics/conv.js
@@ -0,0 +1,30 @@
+let score = "33mil"
+let score1 = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1) //ye integer me
+ //valueInNumber1 = String(score1)//ye string me change kr raha hai 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof score1);
+console.log(score1);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+// "33" => 33//ye change ho rha h  number me 
+//"33abc" => NaN //NaN is also a type of but it cannot convert abc in number
+//true => 1; false =>0 // ye hmko boolean me change kr k dega
+let isLogged ="milan"// value pass krnge to true nhi to false
+
+let booleanisLoggedIn = Boolean(isLogged)
+console.log(booleanisLoggedIn);
+
+let number1 = 33
+let stringNumber = String(number1)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
