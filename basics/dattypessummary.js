e568f9fb6e5dc468a28acc8af99bf6e0ae76d9b0
milanraj2004 <157353158+milanraj2004@users.noreply.github.com>
2025-04-20 06:14:31 +0000

    summary

---
+++ basics/dattypessummary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+//7 types
+/*
+String
+Null
+boolean
+null
+undefined
+symbol
+BigInt
+
+*/
+
+//
+
+//Refrence or Non Primitive
+//Arrays
+//Objects
+//Functions
+let num;
+const num1 =null;
+const num2 =100.05;
+
+const heroes =["Milan","Raj"]
+let myobj = {
+    name: "Milan",
+    age: 20,
+}
+const myfn = function(){
+    console.log("Hello Milan");
+    
+}
+console.log(typeof(num));
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof myfn);
+
+
+
+
