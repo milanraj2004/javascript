a7513adec374738d39f7e688f801cd8ea97f0005
milanraj2004 <157353158+milanraj2004@users.noreply.github.com>
2025-05-12 18:02:21 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,7 @@
+const name ="Milan"
+const repoCount =10
+const collegeid ="22BCS12388"
+console.log(`Hello Milan is college ID ${collegeid} and myrepo count is{repoCount}`);
+// New Way for Decleration of String using objects
+const game = new String("Raj")
+console.log(game)
