a68b3339e3d8d6e1e5be976897497d3e361248d4
Milan Raj <157353158+milanraj2004@users.noreply.github.com>
2025-05-29 20:06:01 +0000

    string

--- basics/string.js
+++ basics/string.js
@@ -5,3 +5,13 @@ console.log(`Hello Milan is college ID ${collegeid} and myrepo count is{repoCoun
 // New Way for Decleration of String using objects
 const game = new String("Raj")
 console.log(game)
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt('a'));
+console.log(game.charAt(2));
+const a = game.substring(0,2)
+console.log(a);
+ 
+
+
+
