d78ebe41ea4cb5c6cac5e7be4bdc2621959f4c3b
Milan Raj <157353158+milanraj2004@users.noreply.github.com>
2025-09-06 16:15:59 +0000

    arrays

---
+++ functions/functions.js
@@ -0,0 +1,27 @@
+function sayMyName(){
+    console.log("Milan");
+
+    console.log("Milan");
+    console.log("Milan");
+    console.log("Milan");
+    
+}
+//sayMyName()
+
+// function addTwoNumber(number1,number2){
+//     console.log(number1 + number2);
+    
+// }
+function addTwoNumber(number1,number2){
+   // console.log(number1 + number2);
+   let result  = number1+number2
+   return result
+
+
+    
+ }
+
+
+
+const result = addTwoNumber(3, null)
+console.log("Result",result);
