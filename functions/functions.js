efd417dded30ca3bf81383c16e902f63ebb18935
Milan Raj <157353158+milanraj2004@users.noreply.github.com>
2025-09-06 19:51:05 +0000

    functions

--- functions/functions.js
+++ functions/functions.js
@@ -25,3 +25,32 @@ function addTwoNumber(number1,number2){
 
 const result = addTwoNumber(3, null)
 console.log("Result",result);
+
+function calculateCartPrice(...num1){
+  return num1
+
+
+}
+console.log(calculateCartPrice(200,500,7000,9000));
+
+const user = {
+   username:"Milan",
+   Uid:"22BCS12388"
+
+
+}
+function handleObject(anyObject){
+ console.log(`Username is ${anyObject.username} and uid is ${anyObject.Uid}`);
+  
+
+}
+handleObject(user)
+
+const arr = [200,400,100,600]
+function returnSecondValue(getArray){
+  return getArray[3]
+
+}
+console.log(returnSecondValue(arr));
+console.log(returnSecondValue([900,700,500,400,600,198]));
+
